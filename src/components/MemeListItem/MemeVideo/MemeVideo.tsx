345e5944f021084bc3bbf924b03033eaac08b978
viclafouch <victor.dlf@outlook.fr>
2024-04-09 20:30:15 +0200

    fix: try fix web mobile video

--- src/components/MemeListItem/MemeVideo/MemeVideo.tsx
+++ src/components/MemeListItem/MemeVideo/MemeVideo.tsx
@@ -7,30 +7,10 @@ export type MemeVideoProps = {
   meme: MemeWithVideo
 } & React.ComponentProps<'video'>
 
-const handlePlay = (event: React.SyntheticEvent<HTMLVideoElement, Event>) => {
-  const target = event.target as HTMLVideoElement
-  const targetId = target.getAttribute('data-id')
-
-  const allVideos = Array.from(
-    document.querySelectorAll<HTMLVideoElement>('video')
-  )
-
-  for (const video of allVideos) {
-    const id = video.getAttribute('data-id')
-
-    if (targetId === id) {
-      continue
-    } else {
-      video.pause()
-      video.currentTime = 0
-    }
-  }
-}
-
 const MemeVideo = ({ meme, ...restVideoProps }: MemeVideoProps) => {
   const id = React.useId()
 
-  return <video data-id={id} onPlay={handlePlay} {...restVideoProps} />
+  return <video data-id={id} {...restVideoProps} />
 }
 
 export default MemeVideo
