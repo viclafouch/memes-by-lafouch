3261c3f0ee074cca898cc78f09293bde911dfb21
viclafouch <victor.dlf@outlook.fr>
2024-04-09 22:27:12 +0200

    feat: preload video on scroll

--- src/components/MemeListItem/MemeVideo/MemeVideo.tsx
+++ src/components/MemeListItem/MemeVideo/MemeVideo.tsx
@@ -2,15 +2,62 @@
 
 import React from 'react'
 import { MemeWithVideo } from '@/constants/meme'
+import useIntersectionObserver from '@react-hook/intersection-observer'
 
 export type MemeVideoProps = {
   meme: MemeWithVideo
 } & React.ComponentProps<'video'>
 
+const stopVideo = (videoElement: HTMLVideoElement) => {
+  videoElement.pause()
+}
+
+const handlePlay = (event: React.SyntheticEvent<HTMLVideoElement, Event>) => {
+  const target = event.target as HTMLVideoElement
+  const targetId = target.getAttribute('data-id')
+
+  const allVideos = Array.from(
+    document.querySelectorAll<HTMLVideoElement>('video')
+  )
+
+  for (const video of allVideos) {
+    const id = video.getAttribute('data-id')
+
+    if (targetId === id) {
+      continue
+    } else {
+      stopVideo(video)
+    }
+  }
+}
+
+const isVideoPlaying = (videoElement: HTMLVideoElement) => {
+  return Boolean(
+    videoElement.currentTime > 0 &&
+      !videoElement.paused &&
+      !videoElement.ended &&
+      videoElement.readyState > 2
+  )
+}
+
 const MemeVideo = ({ meme, ...restVideoProps }: MemeVideoProps) => {
   const id = React.useId()
+  const [ref, setRef] = React.useState<HTMLVideoElement | null>(null)
+  const { isIntersecting } = useIntersectionObserver(ref, {
+    rootMargin: '-64px 0px 0px 0px'
+  })
 
-  return <video data-id={id} {...restVideoProps} />
+  React.useEffect(() => {
+    if (ref && isVideoPlaying(ref) && !isIntersecting) {
+      stopVideo(ref)
+    } else if (isIntersecting && ref) {
+      ref.preload = 'auto'
+    }
+  }, [ref, isIntersecting])
+
+  return (
+    <video ref={setRef} data-id={id} onPlay={handlePlay} {...restVideoProps} />
+  )
 }
 
 export default MemeVideo
