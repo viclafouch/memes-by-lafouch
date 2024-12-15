957454c45e94af5bf4abcb00a6848baab1b64aa3
viclafouch <victor.dlf@outlook.fr>
2024-12-15 14:34:55 +0100

    chore: update deps

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -6,7 +6,7 @@ const serverEnvsSchema = z.object({
   AUTH_TWITTER_ACCOUNT_ID_AUTHORIZED: z.string(),
   AUTH_TWITTER_ID: z.string(),
   AUTH_TWITTER_SECRET: z.string(),
-  UPLOADTHING_SECRET: z.string(),
+  UPLOADTHING_TOKEN: z.string(),
   UPLOADTHING_APP_ID: z.string(),
   ALGOLIA_ADMIN_SECRET: z.string()
 })
@@ -17,7 +17,7 @@ const serverEnv = {
     process.env.AUTH_TWITTER_ACCOUNT_ID_AUTHORIZED,
   AUTH_TWITTER_ID: process.env.AUTH_TWITTER_ID,
   AUTH_TWITTER_SECRET: process.env.AUTH_TWITTER_SECRET,
-  UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+  UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
   UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
   ALGOLIA_ADMIN_SECRET: process.env.ALGOLIA_ADMIN_SECRET
 } as const
--- src/uploadthing/index.ts
+++ src/uploadthing/index.ts
@@ -2,5 +2,5 @@ import { UTApi } from 'uploadthing/server'
 import { SERVER_ENVS } from '@/constants/env'
 
 export const utapi = new UTApi({
-  apiKey: SERVER_ENVS.UPLOADTHING_SECRET
+  token: SERVER_ENVS.UPLOADTHING_TOKEN
 })
