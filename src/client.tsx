d682d4fa81549e600386efd04c358ca003a4c346
viclafouch <victor.dlf@outlook.fr>
2025-07-10 11:31:21 +0200

    feat: try speed-insights from vercel

--- src/client.tsx
+++ src/client.tsx
@@ -2,9 +2,12 @@
 import { hydrateRoot } from 'react-dom/client'
 import { z } from 'zod'
 import { RouterClient } from '@tanstack/react-router/ssr/client'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import { createRouter } from './router'
 
 z.config(z.locales.fr())
+
+injectSpeedInsights()
 
 const router = createRouter()
 
