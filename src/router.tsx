ce4b7ae43d39ae5e02faa5824d7c63ea413c1d41
viclafouch <victor.dlf@outlook.fr>
2025-07-12 11:30:53 +0200

    fix: preload intent

--- src/router.tsx
+++ src/router.tsx
@@ -36,7 +36,7 @@ export function createRouter() {
       defaultErrorComponent: ({ error }) => {
         return <ErrorComponent error={error} />
       },
-      defaultPreload: 'viewport'
+      defaultPreload: 'intent'
     }),
     queryClient
   )
