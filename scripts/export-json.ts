916cdf53dc288ea038e14def0e4b8752666ec664
viclafouch <victor.dlf@outlook.fr>
2025-07-22 15:49:52 +0200

    chore: can export db

---
+++ scripts/export-json.ts
@@ -0,0 +1,16 @@
+import fs from 'node:fs/promises'
+import { PrismaClient } from '@prisma/client'
+
+const prismaClient = new PrismaClient()
+
+export const exportJson = async () => {
+  const memes = await prismaClient.meme.findMany({
+    include: {
+      video: true
+    }
+  })
+
+  await fs.writeFile('./memes.json', JSON.stringify(memes, null, 2))
+}
+
+exportJson()
