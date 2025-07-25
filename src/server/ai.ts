0e8ff5da9df72c459d1ede1a7c73d6e721e908a8
viclafouch <victor.dlf@outlook.fr>
2025-07-25 13:41:40 +0200

    fix: comment ai until zod4 is compatible

--- src/server/ai.ts
+++ src/server/ai.ts
@@ -1,57 +1,59 @@
-import { cosineSimilarity } from 'ai'
-import OpenAI from 'openai'
-import * as R from 'remeda'
+// import * as R from 'remeda'
 import { z } from 'zod'
 import { prismaClient } from '@/db'
+// import { prismaClient } from '@/db'
 import { createServerFn } from '@tanstack/react-start'
 
-const openai = new OpenAI()
+// const openai = new OpenAI()
 
-async function embed(text: string) {
-  const { data } = await openai.embeddings.create({
-    model: 'text-embedding-3-large',
-    input: text,
-    // eslint-disable-next-line camelcase
-    encoding_format: 'float'
-  })
+// async function embed(text: string) {
+//   const { data } = await openai.embeddings.create({
+//     model: 'text-embedding-3-large',
+//     input: text,
+//     // eslint-disable-next-line camelcase
+//     encoding_format: 'float'
+//   })
 
-  return data[0].embedding
-}
+//   return data[0].embedding
+// }
 
 export const getBestMemes = createServerFn({ method: 'GET' })
   .validator((data) => {
     return z.string().parse(data)
   })
-  .handler(async ({ data: query }) => {
-    const dataQueryNormalized = query.toLowerCase().trim() ?? ''
+  .handler(async () => {
+    // const dataQueryNormalized = query.toLowerCase().trim() ?? ''
 
     const memes = await prismaClient.meme.findMany({
+      take: 5,
       include: {
         video: true,
         embedding: true
       }
     })
 
-    const promptEmbedding = await embed(dataQueryNormalized)
+    return memes
 
-    return R.pipe(
-      memes,
-      R.map((meme) => {
-        const embedding = meme.embedding.at(0)
+    // const promptEmbedding = await embed(dataQueryNormalized)
 
-        return {
-          ...meme,
-          score: embedding
-            ? cosineSimilarity(promptEmbedding, embedding.vector)
-            : 0
-        }
-      }),
-      R.filter((meme) => {
-        return meme.score > 0.4
-      }),
-      R.sortBy((meme) => {
-        return meme.score
-      }),
-      R.take(5)
-    )
+    // return R.pipe(
+    //   memes,
+    //   R.map((meme) => {
+    //     const embedding = meme.embedding.at(0)
+
+    //     return {
+    //       ...meme,
+    //       score: embedding
+    //         ? cosineSimilarity(promptEmbedding, embedding.vector)
+    //         : 0
+    //     }
+    //   }),
+    //   R.filter((meme) => {
+    //     return meme.score > 0.4
+    //   }),
+    //   R.sortBy((meme) => {
+    //     return meme.score
+    //   }),
+    //   R.take(5)
+    // )
   })
