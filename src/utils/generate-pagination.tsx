7c41ac29e6b18d2a1188e221107b72606a3e4d2c
viclafouch <victor.dlf@outlook.fr>
2025-07-26 20:22:21 +0200

    fix: paginator

--- src/utils/generate-pagination.tsx
+++ src/utils/generate-pagination.tsx
@@ -1,9 +1,5 @@
 import type { JSX } from 'react'
-import {
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink
-} from '@/components/ui/pagination'
+import { PaginationItem, PaginationLink } from '@/components/ui/pagination'
 
 export const generatePaginationLinks = (
   currentPage: number,
@@ -12,69 +8,19 @@ export const generatePaginationLinks = (
 ) => {
   const pages: JSX.Element[] = []
 
-  if (totalPages <= 6) {
-    for (let index = 1; index <= totalPages; index += 1) {
-      pages.push(
-        <PaginationItem key={`start-${index}`}>
-          <PaginationLink
-            onClick={() => {
-              return onPageChange(index)
-            }}
-            isActive={index === currentPage}
-          >
-            {index}
-          </PaginationLink>
-        </PaginationItem>
-      )
-    }
-  } else {
-    for (let index = 1; index <= 2; index += 1) {
-      pages.push(
-        <PaginationItem key={`start-${index}`}>
-          <PaginationLink
-            onClick={() => {
-              return onPageChange(index)
-            }}
-            isActive={index === currentPage}
-          >
-            {index}
-          </PaginationLink>
-        </PaginationItem>
-      )
-    }
-
-    if (currentPage > 2 && currentPage < totalPages - 1) {
-      pages.push(<PaginationEllipsis key="start-ellipsis" />)
-      pages.push(
-        <PaginationItem key={`current-${currentPage}`}>
-          <PaginationLink
-            onClick={() => {
-              return onPageChange(currentPage)
-            }}
-            isActive
-          >
-            {currentPage}
-          </PaginationLink>
-        </PaginationItem>
-      )
-    }
-
-    pages.push(<PaginationEllipsis key="end-ellipsis" />)
-
-    for (let index = totalPages - 1; index <= totalPages; index += 1) {
-      pages.push(
-        <PaginationItem key={`end-${index}`}>
-          <PaginationLink
-            onClick={() => {
-              return onPageChange(index)
-            }}
-            isActive={index === currentPage}
-          >
-            {index}
-          </PaginationLink>
-        </PaginationItem>
-      )
-    }
+  for (let index = 1; index <= totalPages; index += 1) {
+    pages.push(
+      <PaginationItem key={`start-${index}`}>
+        <PaginationLink
+          onClick={() => {
+            return onPageChange(index)
+          }}
+          isActive={index === currentPage}
+        >
+          {index}
+        </PaginationLink>
+      </PaginationItem>
+    )
   }
 
   return pages
