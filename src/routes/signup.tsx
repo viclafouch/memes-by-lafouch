10b4d3561e60128679324ccac950945cdd26f367
viclafouch <victor.dlf@outlook.fr>
2025-08-08 18:14:40 +0200

    feat: better background

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -1,6 +1,6 @@
 import { toast } from 'sonner'
 import { z } from 'zod'
-import { GradientBackground } from '@/components/animate-ui/backgrounds/gradient'
+import { StarsBackground } from '@/components/animate-ui/backgrounds/stars'
 import { FormControl, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { LoadingButton } from '@/components/ui/loading-button'
@@ -63,163 +63,161 @@ const RouteComponent = () => {
   })
 
   return (
-    <section className="relative w-full h-screen">
-      <GradientBackground className="absolute inset-0 z-0" />
-      <div className="relative flex h-full items-center justify-center z-10">
-        <div className="flex flex-col items-center gap-6 lg:justify-start">
-          <Link to="/">
-            <img
-              src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg"
-              alt="Memes by Lafouch"
-              className="h-10 dark:invert"
-            />
-          </Link>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault()
-              form.handleSubmit()
-            }}
-            noValidate
-            className="min-w-sm border-muted bg-background flex w-full max-w-sm flex-col items-center gap-y-4 rounded-md border px-6 py-8 shadow-md"
-          >
-            <h1 className="text-xl font-semibold">Inscription</h1>
-            <form.Field
-              name="name"
-              children={(field) => {
-                const errorMessage = getFieldErrorMessage({ field })
-
-                return (
-                  <FormItem error={errorMessage}>
-                    <FormControl>
-                      <Input
-                        required
-                        type="text"
-                        id="name"
-                        autoComplete="username"
-                        placeholder="Pseudo"
-                        name="name"
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(event) => {
-                          return field.handleChange(event.target.value)
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            />
-            <form.Field
-              name="email"
-              children={(field) => {
-                const errorMessage = getFieldErrorMessage({ field })
-
-                return (
-                  <FormItem error={errorMessage}>
-                    <FormControl>
-                      <Input
-                        required
-                        type="email"
-                        id="email"
-                        autoComplete="email"
-                        placeholder="Email"
-                        name="email"
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(event) => {
-                          return field.handleChange(event.target.value)
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            />
-            <form.Field
-              name="password"
-              children={(field) => {
-                const errorMessage = getFieldErrorMessage({ field })
-
-                return (
-                  <FormItem error={errorMessage}>
-                    <FormControl>
-                      <Input
-                        required
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                        placeholder="Mot de passe"
-                        name="password"
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(event) => {
-                          return field.handleChange(event.target.value)
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            />
-            <form.Field
-              name="confirmPassword"
-              children={(field) => {
-                const errorMessage = getFieldErrorMessage({ field })
-
-                return (
-                  <FormItem error={errorMessage}>
-                    <FormControl>
-                      <Input
-                        required
-                        type="password"
-                        id="confirmPassword"
-                        autoComplete="new-password"
-                        placeholder="Confirmer le mot de passe"
-                        name="confirmPassword"
-                        value={field.state.value}
-                        onBlur={field.handleBlur}
-                        onChange={(event) => {
-                          return field.handleChange(event.target.value)
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )
-              }}
-            />
-            <form.Subscribe
-              selector={(state) => {
-                return state.isSubmitting
-              }}
-              children={(isSubmitting) => {
-                return (
-                  <LoadingButton
-                    isLoading={isSubmitting}
-                    type="submit"
-                    className="w-full"
-                  >
-                    Créer un compte
-                  </LoadingButton>
-                )
-              }}
-            />
-          </form>
-          <div className="text-gray-300 flex justify-center gap-1 text-sm">
-            <p>Déjà un utilisateur ?</p>
-            <Link
-              to="/login"
-              className="text-secondary font-medium hover:underline"
-            >
-              Se connecter
+    <StarsBackground className="flex items-center justify-center">
+      <section className="relative w-full h-screen">
+        <div className="relative flex h-full items-center justify-center z-10">
+          <div className="flex flex-col items-center gap-6 lg:justify-start">
+            <Link to="/">
+              <img
+                src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg"
+                alt="Memes by Lafouch"
+                className="h-10 dark:invert"
+              />
             </Link>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault()
+                form.handleSubmit()
+              }}
+              noValidate
+              className="min-w-sm border-muted bg-background flex w-full max-w-sm flex-col items-center gap-y-4 rounded-md border px-6 py-8 shadow-md"
+            >
+              <h1 className="text-xl font-semibold">Inscription</h1>
+              <form.Field
+                name="name"
+                children={(field) => {
+                  const errorMessage = getFieldErrorMessage({ field })
+
+                  return (
+                    <FormItem error={errorMessage}>
+                      <FormControl>
+                        <Input
+                          required
+                          type="text"
+                          id="name"
+                          autoComplete="username"
+                          placeholder="Pseudo"
+                          name="name"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(event) => {
+                            return field.handleChange(event.target.value)
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )
+                }}
+              />
+              <form.Field
+                name="email"
+                children={(field) => {
+                  const errorMessage = getFieldErrorMessage({ field })
+
+                  return (
+                    <FormItem error={errorMessage}>
+                      <FormControl>
+                        <Input
+                          required
+                          type="email"
+                          id="email"
+                          autoComplete="email"
+                          placeholder="Email"
+                          name="email"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(event) => {
+                            return field.handleChange(event.target.value)
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )
+                }}
+              />
+              <form.Field
+                name="password"
+                children={(field) => {
+                  const errorMessage = getFieldErrorMessage({ field })
+
+                  return (
+                    <FormItem error={errorMessage}>
+                      <FormControl>
+                        <Input
+                          required
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          placeholder="Mot de passe"
+                          name="password"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(event) => {
+                            return field.handleChange(event.target.value)
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )
+                }}
+              />
+              <form.Field
+                name="confirmPassword"
+                children={(field) => {
+                  const errorMessage = getFieldErrorMessage({ field })
+
+                  return (
+                    <FormItem error={errorMessage}>
+                      <FormControl>
+                        <Input
+                          required
+                          type="password"
+                          id="confirmPassword"
+                          autoComplete="new-password"
+                          placeholder="Confirmer le mot de passe"
+                          name="confirmPassword"
+                          value={field.state.value}
+                          onBlur={field.handleBlur}
+                          onChange={(event) => {
+                            return field.handleChange(event.target.value)
+                          }}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )
+                }}
+              />
+              <form.Subscribe
+                selector={(state) => {
+                  return state.isSubmitting
+                }}
+                children={(isSubmitting) => {
+                  return (
+                    <LoadingButton
+                      isLoading={isSubmitting}
+                      type="submit"
+                      className="w-full"
+                    >
+                      Créer un compte
+                    </LoadingButton>
+                  )
+                }}
+              />
+            </form>
+            <div className="text-gray-300 flex justify-center gap-1 text-sm">
+              <p>Déjà un utilisateur ?</p>
+              <Link to="/login" className="font-medium underline">
+                Se connecter
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </StarsBackground>
   )
 }
 
